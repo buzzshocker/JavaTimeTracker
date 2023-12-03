f406e4f4bc416c59f20fa86649f08d612f3f030e
Japman Singh Kohli <jkohli782@gmail.com>
2023-12-02 17:28:02 -0800

    new changes

--- src/TM.java
+++ src/TM.java
@@ -1,5 +1,45 @@
+import java.io.File;
+
 public class TM {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+}
+
+class Log{
+  private static Log log;
+
+  public Log() {
+
+  }
+
+  public void logWrite() {
+
+  }
+
+  public void logRead() {
+
+  }
+
+  public void logDelete() {
+
+  }
+
+  public void logUpdate() {
+
+  }
+
+//  public static Log getInstance(){
+//      if (log == null) {
+//          log = new Log();
+//      }
+//      return log;
+//  }
+
+}
+
+record taskDetails (String name, String time, String stage, String size,
+                           String description) {
 }
